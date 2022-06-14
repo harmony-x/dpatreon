2d5030e1e2e897bf001b14c03651122fb2d521b5
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-14 14:53:35 +0100

    chore : customize breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,18 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1024px',
+      lg2: '1280px',
+      lg3: '1366px',
+      xl: '1440px',
+    },
       colors: {
         white: "#FFFFFF",
         black: "#333333",
