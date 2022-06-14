1b80f7dd7eb982f632bb19ae90c1527ce08d0e6e
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-15 00:51:33 +0100

    chore : add dummy data for how to create section

---
+++ utils/data.ts
@@ -0,0 +1,23 @@
+import { HowtoCardDataType } from "$types/landingpage";
+import LandingPageConnect from "assets/svg/landing_page_connect";
+
+export const howtoData : HowtoCardDataType[] = [
+  {
+    heading: "CONNECT",
+    Icon: LandingPageConnect,
+    title: "Connect Wallet",
+    description: "After taking a glance at our landing page, connecting to your wallet gives you access to start curating web3 contents.",
+  },
+  {
+    heading: "CONNECT",
+    Icon: LandingPageConnect,
+    title: "Connect Wallet",
+    description: "Connect to your device and start using the app.",
+  },
+  {
+    heading: "CONNECT",
+    Icon: LandingPageConnect,
+    title: "Connect Wallet",
+    description: "Connect to your device and start using the app.",
+  },
+]
