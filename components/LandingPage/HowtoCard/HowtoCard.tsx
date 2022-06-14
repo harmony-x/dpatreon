a6a5685d138456f54590c9b41fc6194f4df9e9e4
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-15 00:52:59 +0100

    feat : build card component for how to create section

---
+++ components/LandingPage/HowtoCard/HowtoCard.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { HowtoCardProps } from "./HowtoCard.types";
+
+const HowtoCard : FC<HowtoCardProps> = ({title, heading, description, Icon}) => {
+  return (
+    <article className="border rounded-4xl pt-8 pb-20 px-8 w-full lg:w-1/3">
+      <h4 className="font-medium text-base md:text-lg pb-8 uppercase text-gray2">{heading}</h4>
+      <Icon width={40} height={40} className="mb-10"  />
+      <h5 className="font-medium text-lg md:text-3xl mb-2 text-white">{title}</h5>
+      <p className="font-light text-base md:text-lg text-white opacity-90">{description}</p>
+    </article>
+  )
+}
+
+export default HowtoCard;
---
+++ components/LandingPage/HowtoCard/HowtoCard.types.ts
@@ -0,0 +1,3 @@
+import { HowtoCardDataType } from "$types/landingpage";
+
+export type HowtoCardProps = HowtoCardDataType;
