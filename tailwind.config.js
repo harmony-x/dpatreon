51658cf728dc8f1e3acb58e9cf1c4c863e998205
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-14 12:02:13 +0100

    chore : customize tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        black: "#333333",
+        primary: "#8247E5"
+      },
+      borderRadius: {
+        '4xl': '1.875rem'
+      },
+      height: {
+        '55px': '3.4375rem'
+      }
+    },
   },
   plugins: [],
 }
