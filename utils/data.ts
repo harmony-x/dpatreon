d0f1bddc08c501a230c47d367f58cabb9f9991b6
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-15 02:01:55 +0100

    chore : add data structure for footer links

--- utils/data.ts
+++ utils/data.ts
@@ -1,4 +1,4 @@
-import { HowtoCardDataType } from "$types/landingpage";
+import { HowtoCardDataType, LinkList } from "$types/landingpage";
 import LandingPageConnect from "assets/svg/landing_page_connect";
 
 export const howtoData : HowtoCardDataType[] = [
@@ -20,4 +20,40 @@ export const howtoData : HowtoCardDataType[] = [
     title: "Connect Wallet",
     description: "Connect to your device and start using the app.",
   },
+]
+
+export const footerDataOne : LinkList[] = [
+  {
+    name: 'How it works',
+    link: '/',
+    id: 1
+  },
+  {
+    name: 'Connect Wallet',
+    link: '/',
+    id: 2
+  },
+  {
+    name: 'Join Discord',
+    link: '/',
+    id: 3
+  }
+]
+
+export const footerDataTwo : LinkList[] = [
+  {
+    name: 'Support',
+    link: '/',
+    id: 1
+  },
+  {
+    name: 'Terms of Use',
+    link: '/',
+    id: 2
+  },
+  {
+    name: 'Privacy Policy',
+    link: '/',
+    id: 3
+  }
 ]
