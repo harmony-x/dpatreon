194625016155c7d4cb37e9b96b2a14863a982e01
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-16 18:13:39 +0100

    feat : build FinishProfileCard component

---
+++ components/UserPage/FinishProfileCard/FinishProfileCard.tsx
@@ -0,0 +1,18 @@
+import Button from "$components/Button/Button";
+import UserAvartar from "$svg/user_avartar";
+import { FC } from "react";
+import { FinishProfileCardProps } from "./FinishProfileCard.types";
+
+const FinishProfileCard : FC<FinishProfileCardProps> = ({}) => {
+  return (
+    <article className="pb-5 bg-white border-xs border-opacity-20 border-gray3 w-full flex flex-col items-stretch">
+      <h4 className="border-xs border-opacity-20 border-gray3 px-6 py-4 font-semibold text-black text-base md:text-lg uppercase">Become a creator</h4>
+      <div className="px-6">
+        <p className="text-black opacity-40 text-sm md:text-base mt-4 mb-6">You’re almost there! Complete <br/> your page and take it live.</p>
+        <Button px="px-5" height='h-42px' onClick={() => {}} text='Finish my page' type='card1'  />
+      </div>
+    </article>
+  )
+}
+
+export default FinishProfileCard;
