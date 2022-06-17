ffcacc49e6505e805a5e3bd4956bc82d0d6ddfeb
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-17 03:45:20 +0100

    chore : add search icon for creator search box

---
+++ assets/svg/creators_search_icon.tsx
@@ -0,0 +1,10 @@
+import { SVGProps } from "react";
+
+export default function CreatorsSearchIcon(props: SVGProps<SVGSVGElement>): JSX.Element {
+  return (
+    <svg {...props} width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M19.32 30.24C13.272 30.24 8.40002 25.368 8.40002 19.32C8.40002 13.272 13.272 8.40002 19.32 8.40002C25.368 8.40002 30.24 13.272 30.24 19.32C30.24 25.368 25.368 30.24 19.32 30.24ZM19.32 10.08C14.196 10.08 10.08 14.196 10.08 19.32C10.08 24.444 14.196 28.56 19.32 28.56C24.444 28.56 28.56 24.444 28.56 19.32C28.56 14.196 24.444 10.08 19.32 10.08Z" fill="#B0B3B5"/>
+      <path d="M27.4529 26.2643L34.9961 33.8075L33.8083 34.9952L26.2651 27.452L27.4529 26.2643Z" fill="#B0B3B5"/>
+    </svg>
+  );
+}
