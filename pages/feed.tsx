73d9a75e02fa7f9aa91590557cd9d954500558b1
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-17 03:22:55 +0100

    feat : build feed page

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -1,4 +1,9 @@
+import Button from '$components/Button/Button'
+import FinishProfileCard from '$components/UserPage/FinishProfileCard/FinishProfileCard'
+import ProfileCard from '$components/UserPage/ProfileCard/ProfileCard'
+import FeedPosts from '$layouts/FeedPosts/FeedPosts'
 import Footer from '$layouts/Footer/Footer'
+import ThreeColumnLayout from '$layouts/ThreeColumnLayout/ThreeColumnLayout'
 import UserPageHeader from '$layouts/UserPageHeader/UserPageHeader'
 import UserFooterLogo from '$svg/user_footer_logo'
 import type { NextPage } from 'next'
@@ -17,7 +22,9 @@ const Home: NextPage = () => {
 
       <main className='font-Gilroy lg3:container mx-auto'>
           <UserPageHeader className='px-2 md:px-7 lg:px-16' />
-          <div className="flex items-center px-6 md:px-12 lg:px-24"></div>
+          <div className="px-6 md:px-12 lg:px-24 mt-12">
+            <ThreeColumnLayout centerColumn={<FeedPosts />} leftColumn={<ProfileCard CTAButton={<Button px="px-4 md:px-6" height='h-42px' onClick={() => {}} text='Share page' type='card1' className="w-full"  />} creation="" patrons={0} userName="0x7ceb23fd6bc...619" />} rightColumn={<FinishProfileCard />} />
+          </div>
       </main>
       <Footer theme={{fill: "#333333", text1: "text-black", text2: "text-black", LogoSVG: UserFooterLogo}} />
     </div>
