d2be6411aab9c537c1b278f6d09c63285f860e55
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-17 12:13:58 +0100

    chore : add lock state

--- components/UserPage/PostCard/PostCard.tsx
+++ components/UserPage/PostCard/PostCard.tsx
@@ -1,14 +1,24 @@
+import Button from "$components/Button/Button";
+import PostLockIcon from "$svg/post_lock_icon";
 import Image from "next/image";
 import Link from "next/link";
 import { FC } from "react";
 import { PostCardProps } from "./PostCard.types";
 
-const PostCard : FC<PostCardProps> = ({image, likes, tags, time, title}) => {
+const PostCard : FC<PostCardProps> = ({image, likes, tags, time, title, creatorId, locked}) => {
   return (
     <Link href={'/feed'}>
       <a className="bg-white border-xs border-opacity-10 border-gray3 w-full min-h-386px overflow-hidden">
-        <div className="relative w-full min-h-200px">
-          <Image className="hover:scale-150 transition-transform" layout="fill" src={image} alt={title} />
+        <div className="relative w-full min-h-200px flex items-center flex-col justify-center">
+          <Image className="hover:scale-150 transition-transform absolute top-0 left-0" layout="fill" src={image} alt={title} />
+          {locked && 
+            <>
+            <div className="absolute top-0 left-0 w-full h-full bg-black2 opacity-60 backdrop-filter"/>
+            <PostLockIcon className="mb-2 relative z-10" />
+            <p className="mb-2 text-white text-sm md:text-base text-center font-semibold relative z-10">Unlock this post <br/> by becoming a patron</p>
+            <Button height="h-9" link="creator/2" px="px-8" text="Join now" type="unlock" className=" relative z-10" />
+          </>
+          }
         </div>
         <div className="py-4 pl-6 pr-8 md:pr-16">
           <p className="text-xs font-medium text-black text-opacity-40 mb-1">{time}</p>
