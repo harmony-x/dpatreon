fab4f1b2c7670ffc9286f6b5420df2f2dae749e5
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-17 03:21:45 +0100

    feat : build post tabs component

---
+++ components/PostTabs/PostTabs.tsx
@@ -0,0 +1,41 @@
+import { FC, Fragment, useRef, useEffect, useState } from "react";
+import { Tab } from "@headlessui/react";
+import { PostTabsProps } from "./PostTabs.types";
+
+const PostTabs: FC<PostTabsProps> = ({ leftTabPanel, rightTabPanel }) => {
+
+  return (
+    <Tab.Group as="div" className="c-tab">
+      <Tab.List className='border-opacity-20 border-b-gray3 border-b-xs'>
+        <Tab as={Fragment}>
+          {({ selected }) => (
+            <button
+              className={`text-base lg:text-lg text-black hover:text-opacity-100 pb-sm transition-all outline-none bg-none mr-8 ${selected ? "font-semibold border-b-black border-b-2" : "text-opacity-60 font-light"}`}
+            >
+              All Posts
+            </button>
+          )}
+        </Tab>
+        <Tab as={Fragment}>
+          {({ selected }) => (
+            <button
+            className={` text-base lg:text-lg text-black  hover:text-opacity-100 pb-sm transition-all outline-none bg-none ${selected ? "font-semibold border-b-black border-b-2" : "text-opacity-60 font-light"}`}
+            >
+              Patron posts
+            </button>
+          )}
+        </Tab>
+      </Tab.List>
+      <Tab.Panels>
+        <Tab.Panel className="mt-6">
+          {leftTabPanel}
+        </Tab.Panel>
+        <Tab.Panel className="mt-6">
+          {rightTabPanel}
+        </Tab.Panel>
+      </Tab.Panels>
+    </Tab.Group>
+  );
+};
+
+export default PostTabs;
---
+++ components/PostTabs/PostTabs.types.ts
@@ -0,0 +1,6 @@
+import React from "react"
+
+export type PostTabsProps = {
+  leftTabPanel: React.ReactNode,
+  rightTabPanel: React.ReactNode
+}
