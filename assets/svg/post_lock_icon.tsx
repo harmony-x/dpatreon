2b737a2d2f9c7f151b6406136aed0405c8047407
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-17 12:12:22 +0100

    chore : add post lock icon

---
+++ assets/svg/post_lock_icon.tsx
@@ -0,0 +1,10 @@
+import { SVGProps } from "react";
+
+export default function PostLockIcon(props: SVGProps<SVGSVGElement>): JSX.Element {
+  return (
+    <svg {...props} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12.6667 7.33325H3.33333C2.59695 7.33325 2 7.93021 2 8.66659V13.3333C2 14.0696 2.59695 14.6666 3.33333 14.6666H12.6667C13.403 14.6666 14 14.0696 14 13.3333V8.66659C14 7.93021 13.403 7.33325 12.6667 7.33325Z" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M4.66675 7.33325V4.66659C4.66675 3.78253 5.01794 2.93468 5.64306 2.30956C6.26818 1.68444 7.11603 1.33325 8.00008 1.33325C8.88414 1.33325 9.73198 1.68444 10.3571 2.30956C10.9822 2.93468 11.3334 3.78253 11.3334 4.66659V7.33325" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
