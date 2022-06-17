358c93cd36c18e36ad6a9283695ac6c2b9bc2057
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-17 04:18:42 +0100

    chore : use avartar component

--- layouts/UserPageHeader/UserPageHeader.tsx
+++ layouts/UserPageHeader/UserPageHeader.tsx
@@ -1,4 +1,5 @@
 // import Button from "$components/Button/Button";
+import Avartar from "$components/Avartar/Avartar";
 import SearchBox from "$components/SearchBox/SearchBox";
 import UserAvartar from "$svg/user_avartar";
 import UserHeaderLogo from "$svg/user_header_logo";
@@ -16,8 +17,8 @@ const UserPageHeader : FC<UserPageHeaderProps> = ({className}) => {
         </a>
       </Link>
       <nav className="flex flex-col md:flex-row items-center mt-3 md:mt-0">
-      <SearchBox SearchIcon={UserHeaderSearchIcon} height={'h-9'} className='rounded-4.5xl font-light text-gray5 bg-gray4 pl-9 text-xs' leftPosition="left-3" placeholder="Find a creator" />
-      <UserAvartar className="ml-0 md:ml-6" />
+      <SearchBox SearchIcon={UserHeaderSearchIcon} height={'h-9'} className='rounded-4.5xl font-light text-gray5 bg-gray4 pl-9 text-xs mr-0 md:mr-6' leftPosition="left-3" placeholder="Find a creator" />
+      <Avartar sizeClassNames="h-10 w-10" src="https://res.cloudinary.com/dexg5uy3d/image/upload/v1644112035/choir1_cdq0wx.jpg" />
         {/* <Button px="px-4 md:px-6" height='h-12 md:h-55px' onClick={() => {}} text='Connect Wallet' type='secondary'  /> */}
       </nav>
     </header>
