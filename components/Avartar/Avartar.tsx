849c8ef1218001b6f1c6219aa18415a9e5ba2a58
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-17 09:21:43 +0100

    style : update style

--- components/Avartar/Avartar.tsx
+++ components/Avartar/Avartar.tsx
@@ -3,8 +3,8 @@ import { AvatarProps } from "./Avartar.types";
 
 const Avartar : FC<AvatarProps> = ({sizeClassNames, src}) => {
   return (
-    <div className={`${sizeClassNames} border-gray6 border-xs rounded-50% flex items-center justify-center p-1px`}>
-      <div className={`w-full h-full bg-cover bg-center rounded-full`} style={{backgroundImage: `url(${src})`}}></div>
+    <div className={`${sizeClassNames} border-gray6 border-xs rounded-50% flex items-center justify-center`}>
+      <div className={`${sizeClassNames} bg-cover bg-center rounded-50%`} style={{backgroundImage: `url(${src})`}}></div>
     </div>
   )
 }
