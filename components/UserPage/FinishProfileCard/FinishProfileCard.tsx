1817fe2343328fcf0255439cd0fe9b0f9a33f415
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-17 12:18:45 +0100

    chore : update link

--- components/UserPage/FinishProfileCard/FinishProfileCard.tsx
+++ components/UserPage/FinishProfileCard/FinishProfileCard.tsx
@@ -9,7 +9,7 @@ const FinishProfileCard : FC<FinishProfileCardProps> = ({}) => {
       <h4 className="border-b-xs border-opacity-20 border-gray3 px-6 py-4 font-semibold text-black text-base md:text-lg uppercase">Become a creator</h4>
       <div className="px-6">
         <p className="text-black text-opacity-40 text-sm md:text-base mt-4 mb-6">You’re almost there! Complete <br/> your page and take it live.</p>
-        <Button px="px-5" height='h-42px' link="user/1" text='Finish my page' type='card1'  />
+        <Button px="px-5" height='h-42px' link="/user/1" text='Finish my page' type='card1'  />
       </div>
     </article>
   )
--- components/UserPage/PostCard/PostCard.tsx
+++ components/UserPage/PostCard/PostCard.tsx
@@ -7,6 +7,31 @@ import { PostCardProps } from "./PostCard.types";
 
 const PostCard : FC<PostCardProps> = ({image, likes, tags, time, title, creatorId, locked}) => {
   return (
+    locked ? 
+      <article className="bg-white border-xs border-opacity-10 border-gray3 w-full min-h-386px overflow-hidden">
+        <div className="relative w-full min-h-200px flex items-center flex-col justify-center">
+          <Image className="hover:scale-150 transition-transform absolute top-0 left-0" layout="fill" src={image} alt={title} />
+          {locked && 
+            <>
+            <div className="absolute top-0 left-0 w-full h-full bg-black2 opacity-60 backdrop-filter"/>
+            <PostLockIcon className="mb-2 relative z-10" />
+            <p className="mb-2 text-white text-sm md:text-base text-center font-semibold relative z-10">Unlock this post <br/> by becoming a patron</p>
+            <Button height="h-9" link="/creator/2" px="px-8" text="Join now" type="unlock" className=" relative z-10" />
+          </>
+          }
+        </div>
+        <div className="py-4 pl-6 pr-8 md:pr-16">
+          <p className="text-xs font-medium text-black text-opacity-40 mb-1">{time}</p>
+          <h4 className="text-base md:text-lg text-black font-semibold mt-3 mb-3">{title}</h4>
+          <div className="flex flex-wrap gap-2 mb-5">
+            {tags.map((tag, index) => (
+              <p className="px-2 py-1 bg-gray3 bg-opacity-10 rounded-4xl text-black text-opacity-60 text-xs" key={index}>{tag}</p>
+            ))}
+          </div>
+          <p className="text-black text-opacity-60 text-sm font-light">{likes} Likes</p>
+        </div>
+      </article>
+    :
     <Link href={'/feed'}>
       <a className="bg-white border-xs border-opacity-10 border-gray3 w-full min-h-386px overflow-hidden">
         <div className="relative w-full min-h-200px flex items-center flex-col justify-center">
