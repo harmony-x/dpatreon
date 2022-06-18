2bdf3ae07727fc8dea2d6e303888f151bd05a4f0
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-18 21:14:20 +0100

    style : update styles

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,11 +5,11 @@ import { ButtonProps } from "./Button.types";
 const Button : FC<ButtonProps> = ({onClick, text, type, className, disabled, height, px, link}) => {
   return (
     !link ? 
-    <button onClick={onClick} disabled={disabled} className={`not-italic rounded-4xl flex items-center justify-center font-Gilroy ${type === "primary" ? "bg-primary text-white text-base font-semibold" : type === "secondary" ?  " bg-white text-black text-base font-semibold" : type === "card1"  ?  "text-sm text-primary bg-lightPurple font-semibold" : "text-sm text-white bg-lightPurple font-semibold bg-opacity-40"} ${className || " "} ${height} ${px}`}>
+    <button onClick={onClick} disabled={disabled} className={`not-italic rounded-4xl flex items-center justify-center font-Gilroy ${type === "primary" ? "bg-primary text-white text-base font-semibold" : type === "secondary" ?  " bg-white text-black text-base font-semibold" : type === "card1"  ?  "text-sm text-primary bg-lightPurple font-medium" : "text-sm text-white bg-lightPurple font-semibold bg-opacity-40"} ${className || " "} ${height} ${px}`}>
       {text}
     </button> : 
     <Link href={link}>
-      <a className={`not-italic rounded-4xl flex items-center justify-center font-Gilroy ${type === "primary" ? "bg-primary text-white text-base font-semibold" : type === "secondary" ?  " bg-white text-black text-base font-semibold" : type === "card1"  ?  "text-sm text-primary bg-lightPurple font-semibold" : "text-sm text-white bg-lightPurple font-semibold bg-opacity-40"} ${className || " "} ${height} ${px}`}>
+      <a className={`not-italic rounded-4xl flex items-center justify-center font-Gilroy ${type === "primary" ? "bg-primary text-white text-base font-semibold" : type === "secondary" ?  " bg-white text-black text-base font-semibold" : type === "card1"  ?  "text-sm text-primary bg-lightPurple font-medium" : "text-sm text-white bg-lightPurple font-semibold bg-opacity-40"} ${className || " "} ${height} ${px}`}>
         {text}
       </a>
     </Link>
