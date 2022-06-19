336fa7e50fae1755d86f6c12aec06ca37a5b2c61
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-19 12:03:14 +0100

    chore : update code

--- assets/svg/ethereum.tsx
+++ assets/svg/ethereum.tsx
@@ -2,7 +2,7 @@ import { SVGProps } from "react";
 
 export default function Ethereum(props?: SVGProps<SVGSVGElement>): JSX.Element {
   return (
-    <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg {...props} width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g clipPath="url(#clip0_42_131)">
       <path d="M19.9407 26.4667L11.9595 21.7566L19.9358 33.0008L27.9211 21.7566L19.9358 26.4667H19.9407ZM20.0593 7L12.0814 20.2413L20.0593 24.9595L28.0405 20.2462L20.0593 7Z" fill="#222222" fillOpacity="0.15"/>
       </g>
