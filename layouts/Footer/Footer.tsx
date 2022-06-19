b348fba9f48ae61faa5ac59efd7fa4dbc1beb6e7
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-19 10:57:23 +0100

    style : remove custom fonts

--- layouts/Footer/Footer.tsx
+++ layouts/Footer/Footer.tsx
@@ -9,20 +9,20 @@ import { FooterProps } from "./Footer.types";
 
 const Footer : FC<FooterProps> = ({theme: {fill, text1, text2, LogoSVG}}) => {
   return (
-    <footer className="font-Gilroy lg3:container mx-auto border-t-xs border-opacity-10 border-gray3">
+    <footer className="lg3:container mx-auto border-t-xs border-opacity-10 border-gray3">
       <div className="flex flex-col md:flex-row md:flex-wrap items-center md:items-start md:justify-between px-6 md:px-12 lg:px-24 pt-16 pb-20 gap-8 md:gap-0">
-        <LogoSVG />
+        <LogoSVG className="w-32 h-10 md:w-40 md:h-14" />
         {[1, 2].map((item) => (
           <nav className="flex flex-col gap-3 items-center md:items-start" key={item}>
           <h5 className={`text-base md:text-lg ${text1} not-italic font-bold`}>{item === 1 ? 'About Us' : 'Help'}</h5>
           {item === 1 && footerDataOne.map(({ id, link, name }) => (
             <Link href={link} key={id}>
-              <a className={`font-light ${text2} text-opacity-60 text-base md:text-lg`}>{name}</a>
+              <a className={`font-light ${text2} text-opacity-60 text-sm md:text-base`}>{name}</a>
             </Link>
           ))}
           {item === 2 && footerDataTwo.map(({ id, link, name }) => (
             <Link href={link} key={id}>
-              <a className={`font-light ${text2} text-opacity-60 text-base md:text-lg`}>{name}</a>
+              <a className={`font-light ${text2} text-opacity-60 text-sm md:text-base`}>{name}</a>
             </Link>
           ))}
         </nav>
@@ -31,7 +31,7 @@ const Footer : FC<FooterProps> = ({theme: {fill, text1, text2, LogoSVG}}) => {
           <div className="flex flex-col gap-3 items-center md:items-start">
             <h5 className={`text-base md:text-lg ${text1} not-italic font-bold`}>Contact Us</h5>
             <Link href={'/'}>
-              <a className={`font-light ${text2} text-opacity-60 text-base md:text-lg`}>support@Patreon3.com</a>
+              <a className={`font-light ${text2} text-opacity-60 text-sm md:text-base`}>support@Patreon3.com</a>
             </Link>
           </div>
           <div className="flex gap-4">
@@ -43,7 +43,7 @@ const Footer : FC<FooterProps> = ({theme: {fill, text1, text2, LogoSVG}}) => {
       </div>
       <p className="border-t-xs border-t-footerBorderColor border-opacity-60 py-6 text-center flex justify-center items-center gap-4">
         <FooterCopyright pathFill={fill} /> 
-        <span className={`font-light ${text2} text-opacity-60 text-base md:text-lg`}>2022 Patreon3. All Rights Reserved</span>
+        <span className={`font-light ${text2} text-opacity-60 text-sm md:text-base`}>2022 Patreon3. All Rights Reserved</span>
       </p>
     </footer>
   )
