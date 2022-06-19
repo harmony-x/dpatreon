08df53660ac0576271703134aa17a78d092bec7c
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-19 10:56:15 +0100

    style : change width for tablet breakpoints

--- layouts/ThreeColumnLayout/ThreeColumnLayout.tsx
+++ layouts/ThreeColumnLayout/ThreeColumnLayout.tsx
@@ -4,9 +4,9 @@ import { ThreeColumnLayoutProps } from "./ThreeColumnLayout.types";
 const ThreeColumnLayout : FC<ThreeColumnLayoutProps> = ({leftColumn, centerColumn, rightColumn}) => {
   return (
     <section className="flex flex-col md:flex-row gap-6 justify-between items-center md:items-start">
-      <div className="w-full md:w-1/4">{leftColumn}</div>
-      <div className="w-full md:w-1/2">{centerColumn}</div>
-      <div className="w-full md:w-1/4">{rightColumn}</div>
+      <div className="w-full md:w-1/3 lg:w-1/4">{leftColumn}</div>
+      <div className="w-full md:w-2/5 lg:w-1/2">{centerColumn}</div>
+      <div className="w-full md:w-1/3 lg:w-1/4">{rightColumn}</div>
     </section>
   )
 }
