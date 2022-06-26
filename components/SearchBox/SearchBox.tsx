eb13a58b87abba8ff57a675856f77078783e91f8
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-26 12:46:06 +0100

    style : update styles

--- components/SearchBox/SearchBox.tsx
+++ components/SearchBox/SearchBox.tsx
@@ -3,7 +3,7 @@ import { SearchBoxProps } from "./SearchBox.types";
 
 const SearchBox : FC<SearchBoxProps> = ({children, height, placeholder, SearchIcon, className, rightSpacing, leftPosition, onChange, containerClassName}) => {
   return (
-    <div className={`${containerClassName} relative mx-auto`}>
+    <div className={`${containerClassName || ""} relative mx-auto`}>
       <div className="relative mb-4 md:mb-0">
         <input onChange={onChange} placeholder={placeholder} className={`${className || ""} ${rightSpacing || "" } ${height}`} />
         {<SearchIcon className={`absolute ${leftPosition} top-1/2 -translate-y-1/2`} width={42}/>}
