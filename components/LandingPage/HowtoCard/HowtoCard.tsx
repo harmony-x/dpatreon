2e524b6af8584e8a3bd2e7a7ef70cc6649dedc47
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-06-26 15:56:43 +0100

    style : update styles

--- components/LandingPage/HowtoCard/HowtoCard.tsx
+++ components/LandingPage/HowtoCard/HowtoCard.tsx
@@ -6,8 +6,8 @@ const HowtoCard : FC<HowtoCardProps> = ({title, heading, description, Icon}) => 
     <article className="border pt-8 pb-20 px-8 w-full lg:w-1/3">
       <h4 className="font-medium text-base md:text-lg pb-8 uppercase text-gray2">{heading}</h4>
       <Icon width={40} height={40} className="mb-10"  />
-      <h5 className="font-medium text-lg md:text-3xl mb-2 text-black">{title}</h5>
-      <p className="font-light text-base md:text-lg text-black text-opacity-90">{description}</p>
+      <h5 className="font-medium text-lg md:text-2xl mb-2 text-black">{title}</h5>
+      <p className="font-light text-sm md:text-base text-black text-opacity-90">{description}</p>
     </article>
   )
 }
