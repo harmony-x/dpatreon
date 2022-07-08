8a589ff17fa544adb05ada7d6b9f2a77c57e929f
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-07-08 02:57:50 +0100

    feat : setup post API endpoint

--- actions.ts
+++ actions.ts
@@ -1,24 +1,45 @@
 import { apiInstance } from "$utils/api";
 
 export interface Tier {
-    benefits: string;
-    description: string;
-    image: string;
-    published: boolean;
-    price: number;
-    title: string;
+  benefits: string;
+  description: string;
+  image: string;
+  published: boolean;
+  price: number;
+  title: string;
 }
 
 export interface Creator {
-    about: string;
-    coverPhoto: string;
-    creatorAddress: string;
-    isAreCreating: string;
-    launched: boolean;
-    name: string;
-    patrons: number;
-    profilePhoto: string;
-    tiers: [Tier, Tier, Tier];
+  about: string;
+  coverPhoto: string;
+  creatorAddress: string;
+  isAreCreating: string;
+  launched: boolean;
+  name: string;
+  patrons: number;
+  profilePhoto: string;
+  tiers: [Tier, Tier, Tier];
 }
 
-export const getCreators = async () => await (await apiInstance.get<Creator[]>("/creators/")).data;
+export interface CreatorPost {
+  data: {
+    image: string;
+    tags: string;
+    text: string;
+    tier: number;
+  };
+  creatorAddress: string;
+}
+
+export const getCreators = async () =>
+  await (
+    await apiInstance.get<Creator[]>("/creators/")
+  ).data;
+
+export const postPost = async (body: CreatorPost) =>
+  await (
+    await apiInstance.post<CreatorPost>(
+      `/posts/${body.creatorAddress}/`,
+      body.data
+    )
+  ).data;
