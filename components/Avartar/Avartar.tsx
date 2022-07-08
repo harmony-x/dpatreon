f2a69b24bbc8a303ad1eb374258baf06a7b2658b
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-07-08 04:31:01 +0100

    style : update style

--- components/Avartar/Avartar.tsx
+++ components/Avartar/Avartar.tsx
@@ -1,12 +1,17 @@
 import { FC } from "react";
 import { AvatarProps } from "./Avartar.types";
 
-const Avartar : FC<AvatarProps> = ({sizeClassNames, src}) => {
+const Avartar: FC<AvatarProps> = ({ sizeClassNames, src }) => {
   return (
-    <div className={`${sizeClassNames} border-gray6 border-xs rounded-50% flex items-center justify-center`}>
-      <div className={`${sizeClassNames} bg-cover bg-center rounded-50%`} style={{backgroundImage: `url(${src})`}}></div>
+    <div
+      className={`${sizeClassNames} relative border-white border-4 bg-center bg-no-repeat bg-cover rounded-50% flex items-center justify-center`}
+      style={{ backgroundImage: `url(${src})` }}
+    >
+      <div
+        className={`${sizeClassNames} absolute -left-1  -top-1 rounded-50% border-gray6 border-xs`}
+      ></div>
     </div>
-  )
-}
+  );
+};
 
 export default Avartar;
