c65b1639c290c407a49e7c62a4a8726df65b6b2e
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-07-11 11:55:49 +0100

    feat : fetch posts user follows

--- layouts/FeedPosts/FeedPosts.tsx
+++ layouts/FeedPosts/FeedPosts.tsx
@@ -1,13 +1,60 @@
 import PostTabs from "$components/PostTabs/PostTabs";
 import EmptyPost from "$components/UserPage/EmptyPost/EmptyPost";
 import PostCard from "$components/UserPage/PostCard/PostCard";
-import { postCardData } from "$utils/data";
+import useLocalStorage from "$hooks/useLocalStorage";
+import { formatDate } from "$utils/function";
+import { useWeb3React } from "@web3-react/core";
+import { getPosts } from "actions";
 import React from "react";
+import { useQuery } from "react-query";
 
-const FeedPosts : React.FC = () => {
+const FeedPosts: React.FC = () => {
+  const {
+    data: creatorPostsData,
+    isLoading: isLoadingCreatorPosts,
+    error: isCreatorPostsError,
+  } = useQuery("creatorPosts", getPosts);
+  const { account } = useWeb3React();
+  const [followings] = useLocalStorage<string[]>("followings");
+  const allPosts = creatorPostsData
+    ?.map(({ author, created_at, id, image, tags, text, tier, likes }) => ({
+      image,
+      tags: tags.split("|"),
+      time: formatDate(created_at),
+      title: text,
+      likes: likes.length,
+      creatorId: author,
+      locked: tier === -1 ? false : true,
+      id,
+      liked: likes.some(({ author }) => author === account) || false,
+    }))
+    .filter(
+      ({ locked, creatorId }) => followings?.includes(creatorId) && !locked
+    );
   return (
-    <PostTabs leftTabPanel={<EmptyPost />} rightTabPanel={<div className='flex flex-col gap-8'>{postCardData.map((item, index) => <PostCard {...item} key={index} />)}</div>} />
-  )
-}
+    <PostTabs
+      tabs={[
+        {
+          name: "All Posts",
+          value: (
+            <div className="flex flex-col gap-8">
+              {allPosts && allPosts.length ? (
+                allPosts.map((item, index) => (
+                  <PostCard {...item} key={index} />
+                ))
+              ) : (
+                <EmptyPost />
+              )}
+            </div>
+          ),
+        },
+        {
+          name: "Patron posts",
+          value: <EmptyPost />,
+        },
+      ]}
+    />
+  );
+};
 
 export default FeedPosts;
