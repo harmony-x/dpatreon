43f677fa4a9d560cfd88ced20b98c33b661ab1d0
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-07-12 09:41:26 +0100

    fix : format time correctly

--- utils/function.ts
+++ utils/function.ts
@@ -3,10 +3,11 @@ import { months } from "./data";
 export const formatDate = (ISO: string) => {
   const date = new Date(ISO);
   const month = date.getMonth() + 1;
-  const hour = new Date(ISO).getHours();
-  return `${months[month - 1]} AT ${hour}:${new Date(ISO).getMinutes()}${
-    hour >= 0 && hour <= 11 ? "AM" : "PM"
-  }`;
+  const hour = new Date(ISO).getHours() + 1;
+  const minutes = new Date(ISO).getMinutes();
+  return `${months[month - 1]} AT ${hour}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }${hour >= 0 && hour <= 11 ? "AM" : "PM"}`;
 };
 
 export const setLocalStorage = <T>(key: string, value: T) => {
